5f03537252096ca564b5df97a8b1baf998801f8d
farne <farne@baa9da6b-ec10-0410-a7e7-fd4f1453c68a>
2009-10-21 09:10:06 +0000

    some very small fixes

--- admin/app/helpers/application_helper.rb
+++ admin/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def migration_status_drop_down object_type
-    content_tag("td", select(object_type,"migration_status", ["Awaiting migration","Ready to migrate","Waiting","Checking failed","Complete","Migration cancelled"]))
+    content_tag("td", select(object_type,"migration_status", ["Awaiting migration","Ready to migrate","Migration failed","Waiting","Checking failed","Complete","Migration cancelled"]))
   end
 end
