3d7300a4d9ef255e9b50c726d773c97664edba08
vasant32 <vasant@ebi.ac.uk>
2013-09-05 16:59:51 +0100

    Editing BioSD migration

--- admin/db/migrate/011_remove_cols_for_bio_sd.rb
+++ admin/db/migrate/011_remove_cols_for_bio_sd.rb
@@ -22,31 +22,35 @@ class RemoveColsForBioSd < ActiveRecord::Migration
     remove_column :experiments,   :has_processed_data
     remove_column :experiments,   :ae_miame_score
     remove_column :experiments,   :ae_data_warehouse_score
+    drop_table  :array_designs
+    drop_table  :array_designs_experiments
+    drop_table  :array_designs_organisms
+    
   end
 
   def self.down
-    add_column :experiments,   :name
-    add_column :experiments,   :user_id
-    add_column :experiments,   :checker_score
-    add_column :experiments,   :software
-    add_column :experiments,   :data_warehouse_ready
-    add_column :experiments,   :date_last_edited
-    add_column :experiments,   :in_curation
-    add_column :experiments,   :curator
-    add_column :experiments,   :experiment_type
-    add_column :experiments,   :miamexpress_login
-    add_column :experiments,   :miamexpress_subid
-    add_column :experiments,   :is_affymetrix
-    add_column :experiments,   :is_mx_batchloader
-    add_column :experiments,   :miame_score
-    add_column :experiments,   :in_data_warehouse
-    add_column :experiments,   :submitter_description
-    add_column :experiments,   :curated_name
-    add_column :experiments,   :num_hybridizations
-    add_column :experiments,   :has_raw_data
-    add_column :experiments,   :has_processed_data
-    add_column :experiments,   :ae_miame_score
-    add_column :experiments,   :ae_data_warehouse_score
+    add_column :experiments,   :name,   :string
+    add_column :experiments,   :user_id,        :integer
+    add_column :experiments,   :checker_score,  :integer
+    add_column :experiments,   :software,       :string  
+    add_column :experiments,   :data_warehouse_ready,   :integer
+    add_column :experiments,   :date_last_edited,       :datetime
+    add_column :experiments,   :in_curation,    :integer
+    add_column :experiments,   :curator,        :string
+    add_column :experiments,   :experiment_type,        :string
+    add_column :experiments,   :miamexpress_login,      :string
+    add_column :experiments,   :miamexpress_subid,      :string
+    add_column :experiments,   :is_affymetrix,  :integer
+    add_column :experiments,   :is_mx_batchloader,      :integer
+    add_column :experiments,   :miame_score,    :integer
+    add_column :experiments,   :in_data_warehouse,      :integer
+    add_column :experiments,   :submitter_description,  :text
+    add_column :experiments,   :curated_name,   :string
+    add_column :experiments,   :num_hybridizations,     :integer
+    add_column :experiments,   :has_raw_data,   :integer
+    add_column :experiments,   :has_processed_data,     :integer
+    add_column :experiments,   :ae_miame_score,  :integer
+    add_column :experiments,   :ae_data_warehouse_score,        :integer
     
   end
 end
