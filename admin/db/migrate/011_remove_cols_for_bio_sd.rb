d673d6725ecfae6697b17922698cd34294f02903
vasant32 <vasant@ebi.ac.uk>
2013-09-05 12:59:07 +0100

    changes to the BioSD database columns

--- admin/db/migrate/011_remove_cols_for_bio_sd.rb
+++ admin/db/migrate/011_remove_cols_for_bio_sd.rb
@@ -1,8 +1,5 @@
 class RemoveColsForBioSd < ActiveRecord::Migration
   def self.up
-  end
-
-  def self.down
     remove_column :experiments,   :name
     remove_column :experiments,   :user_id
     remove_column :experiments,   :checker_score
@@ -25,6 +22,31 @@ class RemoveColsForBioSd < ActiveRecord::Migration
     remove_column :experiments,   :has_processed_data
     remove_column :experiments,   :ae_miame_score
     remove_column :experiments,   :ae_data_warehouse_score
+  end
+
+  def self.down
+    add_column :experiments,   :name
+    add_column :experiments,   :user_id
+    add_column :experiments,   :checker_score
+    add_column :experiments,   :software
+    add_column :experiments,   :data_warehouse_ready
+    add_column :experiments,   :date_last_edited
+    add_column :experiments,   :in_curation
+    add_column :experiments,   :curator
+    add_column :experiments,   :experiment_type
+    add_column :experiments,   :miamexpress_login
+    add_column :experiments,   :miamexpress_subid
+    add_column :experiments,   :is_affymetrix
+    add_column :experiments,   :is_mx_batchloader
+    add_column :experiments,   :miame_score
+    add_column :experiments,   :in_data_warehouse
+    add_column :experiments,   :submitter_description
+    add_column :experiments,   :curated_name
+    add_column :experiments,   :num_hybridizations
+    add_column :experiments,   :has_raw_data
+    add_column :experiments,   :has_processed_data
+    add_column :experiments,   :ae_miame_score
+    add_column :experiments,   :ae_data_warehouse_score
     
   end
 end
